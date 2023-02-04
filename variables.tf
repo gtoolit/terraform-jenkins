54cbef5d40442d1288ab6dc7a44bea738acc9b67
George Toolit <g.toolit@gmail.com>
2023-02-04 14:02:29 +0000

    Update variables

--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "instance_type" {
 variable "instance_keypair" {
     description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
     type = string
-    default = "terraform"
+    default = ""
 }
